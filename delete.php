e967f542cb4ee5609b75223f926b47d3fdb7070f
iJBlade <inesjlbahri@gmail.com>
2020-11-19 11:40:22 +0100

    Mise a jour 19/11/2020 à 11h40

--- delete.php
+++ delete.php
@@ -5,18 +5,34 @@
 /* *********************** * Librairie classe et connexion * ********************************** */
 
     include 'lib/init.php';
-        // pour supprimer un article    
-        $id=$_GET['id']; // $id de l'article détaillé en URL
-        $sql= "DELETE FROM article WHERE id=$id";
-        $req = $bdd->prepare($sql);
-        $req->execute();
+    session_start();
+    if(estConnecte()){
+        // récupération des infos de l'admin
+            $id= $_SESSION["id"];
+            $user = new admin($id);
+        if(isset($_GET["id"])){
+            $id=$_GET['id']; // $id de l'article détaillé en URL
+            $sql= "DELETE FROM article WHERE id=$id";
+            $req = $bdd->prepare($sql);
+            $req->execute();
 
-        // pour supprimer un produit
-        $id_prod=$_GET["prod"];
-        $sql2= "DELETE FROM produit WHERE id=$id_prod";
-        $req2 = $bdd->prepare($sql2);
-        $req2->execute();
+        }
+        if(isset($_GET["prod"])){
+            $id_prod=$_GET["prod"];
+            $sql2= "DELETE FROM produit WHERE id=$id_prod";
+            $req2 = $bdd->prepare($sql2);
+            $req2->execute();
+        }
+        if(isset($_GET["chronique"])){
+            $id_chronique=$_GET["chronique"];
+            $sql3= "DELETE FROM chronique WHERE id=$id_chronique";
+            $req3 = $bdd->prepare($sql3);
+            $req3->execute();
 
-header("Location:dashboard.php");
+        }
+        header("Location:dashboard.php");
 
-        
+    }else{
+        $message = "Si vous n'êtes pas un(e) administrateur(trice), vous n'êtes pas autorisé a consulter cette page. Si vous pensez qu'il s'agit d'un erreur, contactez le(a) développeur(euse).";
+        include'templates/pages/error.php';
+    }
--- templates/fragments/dashboard_offline_chronique.php
+++ templates/fragments/dashboard_offline_chronique.php
@@ -1,14 +1,5 @@
-<?php 
-
-/*                                              Fragment : Détail article HORS ligne côté ADMIN                                                    */
-/*
-        Param : Récupérer le détail d'un article HORS LIGNE selon leur id et leur statut;
-        Retour : détail article -> $id; $categorie;
- 
-*/ 
-
-?> 
 <!DOCTYPE html>
+<?php // page du détail d'un article du site de drop shipping ?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -19,51 +10,47 @@
         <link href="css/style.css" rel="stylesheet" type="text/css"/> 
         <link href="css/header.css" rel="stylesheet" type="text/css"/>  
         <link href="css/footer_blog.css" rel="stylesheet" type="text/css"/> 
-        <link href="css/all.css" rel="stylesheet" type="text/css"/>
-        <link href="css/dashboard_article.css" rel="stylesheet" type="text/css"/>                  
-        <title><?= htmlentities($chronique->getTitre())?></title>       
+        <link href="css/chronique.css" rel="stylesheet" type="text/css"/> 
+             
+        <title>Article-<?= htmlentities($chronique->getTitre())?></title>       
     </head>
     <body>
         <?php include 'templates/fragments/header.php'?>
     <body>
-        <main style="position:absolute;top:60px;width:100%; ">
-        
-        <?php foreach($resultat as $chronique){ ?> 
-                <div id="top_column">
-                    <h1>Aperçu de l'article <span>hors ligne</span>.</h1>
-                    <p>Note : Cette page est l'aperçu de ce que la(e) visitrice(eur) voit lorsqu'il/elle consulte cet article.</p>
-                </div>   
-                <div id="medium_column"style="display:flex;">
-                    <div id="medium_column2">
-                        <div id="medium_column_parent">
-                                <div id="medium_child">
-                                <p id="p_anim"><a id="anim" href="#">Portfolio</a></p><!-- titre  php-->
-                                </div>
-                                <div id="medium_child2">
-                                    <h1><?= htmlentities($chronique->getTitre())?></h1>
-                                    <div id="border"></div>
-                                    <p><?= htmlentities($chronique->getContenu())?></p><!-- titre  contenu-->   
-                                    <p><?= htmlentities($chronique->getContenu2())?></p><!-- titre  contenu-->   
-                                    <p><?= htmlentities($chronique->getContenu3())?></p><!-- titre  contenu-->   
-                                    <p><?= htmlentities($chronique->getContenu4())?></p><!-- titre  contenu-->   
-                                </div>                                   
-                        </div> 
-                        <div id="medium_column_parent2">
-                                    <a href="delete.php?chronique=<?=$chronique->getId()?>" >Supprimer cet article ? </a><br>
-                                    <a href="online.php?chronique=<?=$chronique->getId()?>" >Mettre cet article en ligne ? </a><br>
-                                    <a href="update.php?chronique=<?=$chronique->getId()?>" >Modifier cet article ?</a>                
-                        </div> 
-                    </div>                            
+        <main>
+            <div id="top_column">
+                <h1>Aperçu de l'article hors ligne. </h1>
+                <p>Note : Cette page est l'aperçu de ce que la(e) visitrice(eur) voit lorsqu'il/elle consulte cet article. Toutes modifications sur un article en ligne sera immédiatement vu par la(e) visitrice(eur).</p>
+            </div>         
+        <?php foreach($resultat as $chronique){ ?>   
+                <div id="container_head">
+                    <div id="head">
+                        <h1><?= htmlentities($chronique->getTitre()) ?></h1>
+                        <h4>Date-Catégories</h4>
+
+                    </div>
                     <div id="slider">                                                         
                         <img src="photochronique1/<?= htmlentities($chronique->getId())?>.jpg" class="slide1" />                                     
                         <img src="photochronique2/<?= htmlentities($chronique->getId())?>.jpg" class="slide2" />                                                                         
                         <img src="photochronique3/<?= htmlentities($chronique->getId())?>.jpg" class="slide3" />                                     
                     </div>
-                </div>
+                </div>   
+                <div id="container_bottom">
+                        <p><?= nl2br(htmlentities($chronique->getContenu())) ?></p>
+                        <p><?= nl2br(htmlentities($chronique->getContenu2())) ?></p>
+                        <p><?= nl2br(htmlentities($chronique->getContenu3())) ?></p>
+                        <p><?= nl2br(htmlentities($chronique->getContenu4())) ?></p>
+                        <div>
+                            <a href="delete.php?chronique=<?=$chronique->getId()?> " id="aA">Supprimer cet article ? </a><br>
+                            <a href="offline.php?chronique=<?=$chronique->getId()?>" id="aA" >Mettre cet article hors ligne ? </a><br>
+                            <a href="update.php?chronique=<?=$chronique->getId()?>"  id="aA">Modifier cet article ?</a>   
+                        </div>
+                </div>   
                 <?php } ?>
         </main>
-        <?php  include "templates/fragments/footer.php"?>                    
+        <?php//  include "templates/fragments/footer.php"?>                    
     </body>
 </html>
 <script src="js/header.js" type="text/javascript"></script>
 <script src="js/all.js" type="text/javascript"></script>
+
